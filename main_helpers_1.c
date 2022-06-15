55b01faf7a411362e1e9ffc02b394b7509fbda3c
JumaOchi <ochiengjuma95@gmail.com>
2022-06-15 14:01:16 -0700

    made corrections on file helpers_1 with updates

--- main_helpers_1.c
+++ main_helpers_1.c
@@ -10,152 +10,107 @@
 #include <signal.h>
 
 /**
- * handle_cmd - function that dispatches the command lines to handle_path_cmd
- * or to handle_nopath_cmd, depending on whether the command lines consist of
- * strings compounded with the PATH directory or of strings without the PATH
- * directory
- * @av: double pointer to ./file_name (accessed via av[0]) to be passed on to
- * other functions
- * @counter: tracking the number of commands that have been executed or failed
- * @array_buf: array_buf: pointer to array of pointers to the args passed in by
- * the user on standard input
- * @array_path: pointer to array of pointers to the PATH directories
- * @str_buf_path: pointer to the command argument (array_buf[0] or
- * concatenated string of command with array_path directory strings
- * Return: exit status, return value of the fork_n_exec function (0 or -1) to
- * be passed to the exit_ built-in if called
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: string of chars
+ * Return: address of the newly allocated memory
  */
 
-int handle_cmd(char **av, int counter, char **array_buf, char **array_path,
-	       char *str_buf_path)
+char *_strdup(char *str)
 {
-	int k = 0, flag = 0, exit_status = 0;
+	unsigned int len = 0;
+	unsigned int i = 0, j = 0;
+	char *str_copy = NULL;
+	char *tmp = str;
 
-	while (array_buf[0][k] != '\0')
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*str++)
+		i++;
+	len = i;
+	str = tmp;
+
+	str_copy = malloc(len * sizeof(char) + 1);
+	if (str_copy == NULL)
+		return (NULL);
+
+	j = 0;
+	while (j < len)
 	{
-		if (array_buf[0][k] == '/')
-		{
-			flag = 1;
-			break;
-		}
-		k++;
+		str_copy[j] = str[j];
+		j++;
 	}
-	if (flag == 1)
-		exit_status = handle_path_cmd(av, counter, array_buf), flag = 0;
-	else if (flag == 0)
-		exit_status = handle_nopath_cmd(array_path, av, array_buf,
-						counter, str_buf_path);
-	return (exit_status);
+	str_copy[j] = '\0';
+	return (str_copy);
 }
 
 /**
- * handle_path_cmd - function that handles command lines consisting of strings
- * compounded with the PATH directory
- * @av: double pointer to ./file_name (accessed via av[0]) to be passed on to
- * other functions
- * @counter: tracking the number of commands that have been executed or failed
- * @array_buf: array_buf: pointer to array of pointers to the args passed in by
- * the user on standard input
- * Return: exit status, return value of the fork_n_exec function (0 or -1) to
- * be passed to the exit_ built-in if called
+ * _memset - function that fills memory with a constant byte
+ * @s: parameter defined in main, pointer to memory area
+ * @b: parameter defined in main, constant byte
+ * @n: parameter defined in main, number of bytes to be filled
+ * Return: memory address of function (memory area)
  */
 
-int  handle_path_cmd(char **av, int counter, char **array_buf)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int status = 0, exit_status = 0;
+	unsigned int i;
+	char *tmp = s;
 
-	status = _status(array_buf[0]);
-	if (status == -1)
+	for (i = 0; i < n; i++)
 	{
-		_print(av[0]);
-		_print(": ");
-		print_number(counter);
-		_print(": ");
-		_print(array_buf[0]);
-		_print(": not found\n");
-		exit_status = 127;
-		free(array_buf);
+		*s = b;
+		s++;
 	}
-	else if (status == 0)
-	{
-		exit_status = fork_n_exec(array_buf[0], array_buf);
-		free(array_buf);
-	}
-	return (exit_status);
+	s = tmp;
+	return (s);
 }
 
 /**
- * handle_nopath_cmd - function that handles command lines consisting of
- * strings without the PATH directory
- * @array_path: pointer to array of pointers to the PATH directories
- * @av: double pointer to ./file_name (accessed via av[0]) to be passed on to
- * other functions
- * @array_buf: array_buf: pointer to array of pointers to the args passed in by
- * the user on standard input
- * @counter: tracking the number of commands that have been executed or failed
- * @str_buf_path: pointer to the command argument (array_buf[0] or
- * concatenated string of command with array_path directory strings
- * Return: exit status, return value of the fork_n_exec function (0 or -1) to
- * be passed to the exit_ built-in if called
+ * _strcat - function that concatenates two strings
+ * @dest: string passed by main or by another function
+ * @src: string passed by main or by another function
+ * Return: pointer to the concatenated string
  */
 
-int handle_nopath_cmd(char **array_path, char **av, char **array_buf,
-		      int counter, char *str_buf_path)
+char *_strcat(char *dest, char *src)
 {
-	int exit_status = 0;
+	char *tmp = NULL;
 
-	if (array_path != NULL && array_path[0] != NULL)
+	tmp = dest;
+	while (*dest != '\0')
 	{
-		str_buf_path = buf_path_cat(counter, av[0], array_buf[0], array_path);
-		if (str_buf_path != NULL)
-		{
-			exit_status = fork_n_exec(str_buf_path, array_buf);
-			free(str_buf_path);
-		}
-		else
-			exit_status = 127;
-		free(array_buf);
+		dest++;
 	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest++ = '\0';
+	dest = tmp;
+	return (dest);
+}
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: pointer to string (or literal string) to be compared
+ * @s2: pointer to string (or literal string) to be compared
+ * Return: 0 if the two strings are identical, 1 otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
 	else
-		free(array_buf);
-	return (exit_status);
-}
-
-/**
- * fork_n_exec - program that executes a command
- * in a child process
- * @str_buf_path: pointer to the command argument (array_buf[0] or
- * concatenated string of command with array_path directory strings)
- * @array_buf: pointer to array of pointers to the args passed in by the user
- * on standard input
- * Return: 0 if execve found the executable of the command, -1 if execve did
- * not find the executable of the command
- */
-
-int fork_n_exec(char *str_buf_path, char **array_buf)
-{
-	int exec_status = 0, exit_status = 0;
-	pid_t pid;
-	int status;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Error");
-		exit(1);
-	}
-	if (pid == 0)
-	{
-		exec_status = execve(str_buf_path, array_buf, environ);
-		if (exec_status == -1)
-		{
-			perror(array_buf[0]);
-			exit(127);
-		}
-	}
-	else
-		wait(&status);
-	if (WIFEXITED(status))
-		exit_status = WEXITSTATUS(status);
-	return (exit_status);
+		return (1);
 }
